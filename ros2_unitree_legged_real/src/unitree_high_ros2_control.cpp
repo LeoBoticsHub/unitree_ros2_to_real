8ddd3eef3aaa04f140c9ec8f025d5572f24ab650
Federico Rollo <rollo.f96@gmail.com>
2024-03-22 13:41:28 +0100

    initialized high controller for ros2

---
+++ ros2_unitree_legged_real/src/unitree_high_ros2_control.cpp
@@ -0,0 +1,19 @@
+/*
+** Copyright (C) 2024 Federico Rollo, Leonardo Innovation Labs - All Rights Reserved
+**/
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "unitree_legged_sdk/unitree_legged_sdk.h"
+
+#include "ros2_unitree_legged_msgs/srv/get_battery_state.hpp"
+#include "ros2_unitree_legged_msgs/srv/get_float.hpp" 
+#include "ros2_unitree_legged_msgs/srv/get_int.hpp" 
+#include "ros2_unitree_legged_msgs/srv/set_body_orientation.hpp"
+#include "ros2_unitree_legged_msgs/srv/set_float.hpp"
+#include "ros2_unitree_legged_msgs/srv/set_int.hpp"
+
+int main()
+{
+    return 0;
+}
